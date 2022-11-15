dbbc41e2662cd1574501b3422e5db8caf199510b
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-15 10:15:53 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,44 @@
+var WidthRect = 70;
+var heightRect = 70;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    background(220)
 }
 
 function draw() {
-    background(220);
+    
+}
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+function mousePressed() {
+
+    if(mouseButton == LEFT){
+        fill(random(0,255), random(0,255), random(0,255))
+    rectMode(CENTER)
+    rect(mouseX, mouseY, WidthRect, heightRect);
+    }
+
+    if(mouseButton == RIGHT){
+        fill(random(0,255), random(0,255), random(0,255))
+    ellipseMode(CENTER)
+    ellipse(mouseX, mouseY, WidthRect, heightRect);
+    }
+}
+
+function keyPressed() {
+    if(keyCode == ENTER){
+    background(0);
+    }
+
+    if(keyCode == UP_ARROW){
+        fill(random(0,255), random(0,255), random(0,255))
+        rectMode(CENTER)
+        rect(mouseX, mouseY, widthRect+20, heightRect+40)
+    }
+
+    if(keyCode == DOWN_ARROW){
+        fill(random(0,255), random(0,255), random(0,255))
+        triangle(mouseX, mouseY, mouseX+50, mouseY, mouseX+25, mouseY-50)
+    }
 }
